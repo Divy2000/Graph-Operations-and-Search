65c5d626dbfd05262162356f4ac1bd3448691fa6
Divy2000 <39717289+Divy2000@users.noreply.github.com>
2023-02-13 21:25:23 -0700

    Command line interface completed and started testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +14,8 @@ public class Main {
             catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            String nodeLabel, nodeLabels, node1, node2, filepath;
+            String[] nodeLabelList;
             while(running) {
                 System.out.println("\nChoose one of the following options");
                 System.out.println("0 -> Print graph details");
@@ -28,62 +28,74 @@ public class Main {
                 System.out.println("7 -> Remove an edge from the graph");
                 System.out.println("8 -> Output graph to .dot file");
                 System.out.println("9 -> Output graph to an image file");
+                System.out.println("Q -> Exit the software");
                 System.out.println("Enter your choice");
 
-                int choice = inputScanner.nextInt();
+                String choice = inputScanner.nextLine();
                 switch (choice){
-                    case 0:
+                    case "0":
                         String graphDetails = g.toString();
                         System.out.println(graphDetails);
                         break;
-                    case 1:
+                    case "1":
                         System.out.println("Enter the filepath of the output file: ");
                         String outputTxtPath = inputScanner.nextLine();
                         g.outputGraph(outputTxtPath);
                         break;
-                    case 2:
+                    case "2":
                         System.out.println("Enter the label of the node you want to add: ");
-                        String nodeLabel = inputScanner.nextLine();
+                        nodeLabel = inputScanner.nextLine();
                         g.addNode(nodeLabel);
                         break;
-                    case 3:
+                    case "3":
                         System.out.println("Enter the labels of node you want to add separated by comma ('a,b,c'): ");
-                        String nodeLabels = inputScanner.nextLine();
-                        String[] nodeLabelList = nodeLabels.split(",");
+                        nodeLabels = inputScanner.nextLine();
+                        nodeLabelList = nodeLabels.split(",");
                         g.removeNodes(nodeLabelList);
                         break;
-                    case 4:
-                        System.out.println("Enter the first/source node of the link");
+                    case "4":
+                        System.out.println("Enter the first/source node of the edge");
                         inputScanner.nextLine();
-                        String node1 = inputScanner.nextLine();
-                        System.out.println("Enter the second/target node of the link");
-                        String node2 = inputScanner.nextLine();
+                        node1 = inputScanner.nextLine();
+                        System.out.println("Enter the second/target node of the edge");
+                        node2 = inputScanner.nextLine();
                         g.addEdge(node1, node2);
+                        break;
+                    case "5":
+                        System.out.println("Enter the label of the node you want to remove: ");
+                        nodeLabel = inputScanner.nextLine();
+                        g.removeNode(nodeLabel);
+                        break;
+                    case "6":
+                        System.out.println("Enter the labels of node you want to remove separated by comma ('a,b,c'): ");
+                        nodeLabels = inputScanner.nextLine();
+                        nodeLabelList = nodeLabels.split(",");
+                        g.removeNodes(nodeLabelList);
+                        break;
+                    case "7":
+                        System.out.println("Enter the first/source node of the edge");
+                        inputScanner.nextLine();
+                        node1 = inputScanner.nextLine();
+                        System.out.println("Enter the second/target node of the edge");
+                        node2 = inputScanner.nextLine();
+                        g.removeEdge(node1, node2);
+                        break;
+                    case "8":
+                        System.out.println("Enter the path of the output .dot file");
+                        inputScanner.nextLine();
+                        filepath = inputScanner.nextLine();
+                        g.outputDOTGraph(filepath);
+                        break;
+                    case "9":
+                        System.out.print("Enter path to output the image of the final graph : ");
+                        filepath = inputScanner.nextLine();
+                        String[] outputImageDetails = filepath.split("\\.");
+                        g.outputGraphics(filepath, outputImageDetails[1]);
+                        break;
+                    case "Q":
+                        running = false;
                         break;
                 }
             }
-//            System.out.print("Enter the filepath of the output file: ");
-//            String outputTxtPath = inputScanner.nextLine();
-//            System.out.println();
-//            g.parseGraph(graphFilePath);
-//            String graphString = g.toString();
-//            System.out.println(graphString);
-//            g.outputGraph(outputTxtPath);
-//            g.addNode("test");
-//            g.removeNode("green");
-//            String[] nodesToAdd = {"node1", "node2"};
-//            g.addNodes(nodesToAdd);
-//            String[] nodesToRemove = {"test", "node1"};
-//            g.removeNodes(nodesToRemove);
-//            g.addEdge("test", "a");
-//            g.removeEdge("yellow", "red");
-//            System.out.print("Enter the filepath to the output code in '.dot' format: ");
-//            String outputDotPath = inputScanner.nextLine();
-//            System.out.println();
-//            g.outputDOTGraph(outputDotPath);
-//            System.out.print("Enter path to output the image of the final graph : ");
-//            String outputImagePath = inputScanner.nextLine();
-//            String[] outputImageDetails = outputImagePath.split("\\.");
-//            g.outputGraphics(outputImagePath, outputImageDetails[1]);
     }
 }
---
+++ src/test/java/myGraphClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class myGraphClassTest {
+
+    @Test
+    void feature1() {
+        String filepath = "src/main/java/color.dot";
+
+    }
+}
