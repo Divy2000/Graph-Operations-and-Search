15c515ef2ab87feebfafb2db5ae01f8e8ebb6e4b
Divy2000 <39717289+Divy2000@users.noreply.github.com>
2023-03-14 02:01:49 -0700

    Added Path class

---
+++ src/main/Path.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import guru.nidi.graphviz.model.Node;
+
+public class Path {
+    private List<Node> nodes;
+
+    public Path() {
+        this.nodes = new ArrayList<>();
+    }
+
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public boolean contains(Node node) {
+        return nodes.contains(node);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nodes.size(); i++) {
+            sb.append(nodes.get(i).toString());
+            if (i < nodes.size() - 1) {
+                sb.append(" -> ");
+            }
+        }
+        return sb.toString();
+    }
+}
